5d38de4b4107446de52e41cd8286d5485ea5bc06
Abdale Hassan <cabdale989@gmail.com>
2021-07-28 20:26:07 -0500

    create array to store files for caching and declare two variable one for static cache and the other for data cache

---
+++ public/service-worker.js
@@ -0,0 +1,16 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/index.js",
+    "/indexedDB.js",
+    "/manifest.webmanifest",
+  ];
+  
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+  
+  
